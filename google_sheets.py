6ca49b23aeff1c57aa662401dcea1daec6740a45
Sandmanager1234 <sandmanager1234@gmail.com>
2025-03-29 21:18:23 +0300

    first

---
+++ google_sheets.py
@@ -0,0 +1,188 @@
+import gspread
+from gspread_formatting import set_frozen, set_column_width
+import os
+import datetime
+from loguru import logger
+
+MONTH = {
+    1: 'Январь',
+    2: 'Февраль',
+    3: 'Март',
+    4: 'Апрель',
+    5: 'Май',
+    6: 'Июнь',
+    7: 'Июль',
+    8: 'Август',
+    9: 'Сентябрь',
+    10: 'Октябрь',
+    11: 'Ноябрь',
+    12: 'Декабрь',
+}
+
+LETTERS = {
+    1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E', 6: 'F', 7: 'G', 8: 'H', 
+    9: 'I', 10: 'J', 11: 'K', 12: 'L', 13: 'M', 14: 'N', 15: 'O', 16: 'P', 
+    17: 'Q', 18: 'R', 19: 'S', 20: 'T', 21: 'U', 22: 'V', 23: 'W', 24: 'X', 
+    25: 'Y', 26: 'Z',27: 'AA', 28: 'AB', 29: 'AC', 30: 'AD', 31: 'AE', 32: 'AF'
+}
+
+
+class Constructor:
+    def get_first_col():
+        first_col = []
+        
+        rows_title = [
+            'Кол-во лидов общее',
+            'Кол-во лидов таргет',
+            'Кол-во лидов звонобот',
+            'Кол-во лидов прочее',
+            '',
+            'Кол-во обработанных лидов',
+            'Кол-во квал лидов',
+            '% Квалификации',
+            '',
+            'Кол-во успешек',
+            'Конверсия из лида в продажу',
+            'Конверсия из квал-лида в продажу',
+            ''
+        ]
+        for city in ['АЛМАТА', 'АСТАНА', 'ОНЛАЙН']:
+            first_col.append(city)
+            first_col.extend(rows_title)
+        return [first_col]
+    
+    def get_date_row():
+        month = str(datetime.datetime.now().month).zfill(2)
+        return [f'{str(i).zfill(2)}.{month}' for i in range(1, 32)]
+    
+    def get_formules_rows():
+        formulas = {}
+        for row_index in [1, 15, 29]:
+            formulas[row_index] = Constructor.get_date_row()
+        for row_id in [2, 16, 30]:
+            formulas[row_id] = []
+            for i in range(1, 32):
+                formulas[row_id].append(f'=СУММ({LETTERS[i]}{row_id+1}:{LETTERS[i]}{row_id+3})')
+        for row_id in [9, 23, 37]:
+            formulas[row_id] = []
+            for i in range(1, 32):
+                formulas[row_id].append(f'={LETTERS[i]}{row_id-1}/{LETTERS[i]}{row_id-2}')
+        for row_id in [9, 23, 37]:
+            formulas[row_id] = []
+            for i in range(1, 32):
+                formulas[row_id].append(f'={LETTERS[i]}{row_id-1}/{LETTERS[i]}{row_id-2}')
+        for row_id in [12, 26, 40]:
+            formulas[row_id] = []
+            for i in range(1, 32):
+                formulas[row_id].append(f'={LETTERS[i]}{row_id-1}/{LETTERS[i]}{row_id-10}')
+        for row_id in [13, 27, 41]:
+            formulas[row_id] = []
+            for i in range(1, 32):
+                formulas[row_id].append(f'={LETTERS[i]}{row_id-5}/{LETTERS[i]}{row_id-11}')
+        return formulas
+
+
+class GoogleSheets:
+
+    def __init__(self):
+        try:
+            logger.info("Инициализация GoogleSheets")
+            gc = gspread.service_account(filename="credentials.json")
+            self.table = gc.open_by_key(os.getenv("table_id"))
+            # self.ws = self.create_new_sheet()
+            logger.info("Успешное подключение к таблице")
+        except Exception as e:
+            logger.error(f"Ошибка при инициализации GoogleSheets: {e}")
+            raise
+
+    def get_sheet(self, timestamp: int = None):
+        if not timestamp:
+            current_date = datetime.datetime.now()
+        sheet_name = f'{MONTH[current_date.month]} {current_date.year}'
+        try:
+            ws = self.table.worksheet(sheet_name)
+        except Exception as ex:
+            logger.warning(f'Лист {sheet_name} не найден. Ошибка: {ex}')
+            ws = self.create_new_sheet(sheet_name)
+        return ws
+
+    def create_new_sheet(self, sheet_name):
+        try:
+            ws = self.table.add_worksheet(sheet_name, 42, 42)
+            formulas = Constructor.get_formules_rows()
+            rows = []
+            for index in range(1, 43):
+                if index not in formulas:
+                    rows.append([''])
+                else:
+                    rows.append(formulas[index])
+            ws.insert_rows(rows, 1, value_input_option="USER_ENTERED")
+            ws.insert_cols(Constructor.get_first_col(), 1, value_input_option="USER_ENTERED")
+            ws.format(['9:9', '12:12', '13:13', '23:23', '26:26', '27:27', '37:37', '40:40', '41:41'], {
+                'numberFormat': {'type': 'PERCENT'}
+            })
+            ws.format(['2:5', '16:19', '30:33'], {
+                "backgroundColor": {
+                    
+                        "red": 0.7882,
+                        "green": 0.8549,
+                        "blue": 0.9725
+                    
+                },
+            })
+            ws.format(['7:9', '21:23', '35:37'], {
+                "backgroundColor": {
+                "red": 0.851,
+                "green": 0.8235,
+                "blue": 0.9137
+                },
+            })
+            ws.format(['11:13', '25:27', '39:41'], {
+                "backgroundColor": {
+                "red": 0.851,
+                "green": 0.9176,
+                "blue": 0.8275
+                },
+            })
+            ws.format(['A1', 'A15', 'A29'], {
+                "backgroundColor": {
+                "red": 0.9569,
+                "green": 0.8,
+                "blue": 0.8
+                },
+                'textFormat': {
+                    "fontSize": 14,
+                    "bold": True
+                }
+            })
+            set_frozen(ws, cols=1)
+            set_column_width(ws, 'A', 200)
+            return ws
+        except Exception as e:
+            logger.error(f'Ошибка создания таблицы: {e}')
+            raise
+
+    def insert_value(self, row, col, timestamp: int = None):
+        """Вставляет строку данных на указанный индекс."""
+        try:
+            logger.info(f"Вставка +1 на позицию {row}:{col}")
+            ws = self.get_sheet(timestamp)
+            value = ws.cell(row, col).value
+            value = int(value) if value else 0
+            ws.update_cell(row, col, value + 1)
+            logger.info("Значение успешно вставлена")
+        except Exception as e:
+            logger.error(f"Ошибка при вставке значения: {e}")
+            raise
+
+    def minus_value(self, row, col):
+        try:
+            logger.info(f"Вставка -1 на позицию {row}:{col}")
+            ws = self.get_sheet()
+            value = ws.cell(row, col).value
+            value = int(value) if value else 0
+            ws.update_cell(row, col, value - 1)
+            logger.info("Значение успешно вставлена")
+        except Exception as e:
+            logger.error(f"Ошибка при вставке значения: {e}")
+            raise
---
+++ main.py
@@ -0,0 +1,99 @@
+import os
+import asyncio
+from datetime import datetime, timedelta
+from google_sheets import GoogleSheets
+from amocrm.amocrm import AmoCRMClient
+from amocrm.models import Lead, Events
+from dotenv import load_dotenv
+from loguru import logger
+
+from scheduler import Scheduler
+
+# Загрузка переменных из .env файла
+load_dotenv()
+
+SECONDS = int(os.getenv('seconds'))
+# Получение имени текущей директории
+current_directory_name = os.path.basename(os.getcwd())
+
+os.makedirs(".", exist_ok=True)  # Создание папки logs, если её нет
+
+log_file_path = os.path.join(
+    "logs", f"{current_directory_name}_{{time:YYYY-MM-DD}}.log"
+)
+
+
+# Настройка ротации логов
+logger.add(
+    log_file_path,  # Файл лога будет называться по дате и сохраняться в поддиректории с названием текущей директории
+    rotation="00:00",  # Ротация каждый день в полночь
+    retention="7 days",  # Хранение логов за последние 7 дней
+    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}",  # Формат сообщений в файле
+    level="INFO",  # Минимальный уровень логирования
+    compression="zip",  # Архивирование старых логов
+)
+
+google = GoogleSheets()
+amo_client = AmoCRMClient(
+    base_url="https://teslakz.amocrm.ru",
+    access_token=os.getenv("access_token"),
+    client_id=os.getenv("client_id"),
+    client_secret=os.getenv("client_secret"),
+    permanent_access_token=True,
+)
+
+async def processing_leads(events: Events, poll_type: str):
+    for event in events:
+        try:
+            lead_json = await amo_client.get_lead(event.entity_id)
+            if lead_json:
+                lead = Lead.from_json(lead_json, poll_type=poll_type)
+                timestamp = event.created_at
+                if poll_type == 'proccessing':
+                    from_timestamp = (datetime.now() - timedelta(weeks=1)).replace(hour=0, minute=0, microsecond=0, second=0)
+                    events = Events.from_json(await amo_client.get_events_processing_before(lead.id, from_timestamp))
+                    timestamp = events.get_timestamp_by_index()
+                google.insert_value(*lead.get_row_col(timestamp))
+        except Exception as ex:
+            logger.error(f'Ошибка обработки сделки: {ex}')
+
+
+async def polling_leads(timestamp):
+    amo_client.start_session()
+    try:
+
+        tags_events = Events.from_json(await amo_client.get_events_tags(timestamp))
+        await processing_leads(tags_events, 'tags')
+ 
+        success_events = Events.from_json(await amo_client.get_events_success(timestamp))
+        await processing_leads(success_events, 'success')
+        
+        qualified_events = Events.from_json(await amo_client.get_events_qualified(timestamp))
+        await processing_leads(qualified_events, 'qualified')
+
+        from_processing_events = Events.from_json(await amo_client.get_events_from_processing(timestamp))
+        await processing_leads(from_processing_events, 'proccessing')
+
+    except Exception as e:
+        logger.error(f'Ошибка составления запроса: {e}')
+    finally:
+        await amo_client.close_session()
+
+
+async def main():
+    scheduler = Scheduler(polling_leads, SECONDS)
+    while True:
+        try:
+            await scheduler.start()
+            await asyncio.sleep(600)
+        except Exception as ex:
+            logger.error(f'Ошибка: {ex}')
+        finally:
+            await scheduler.stop()
+
+
+# Запуск приложения
+if __name__ == "__main__":
+    
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
